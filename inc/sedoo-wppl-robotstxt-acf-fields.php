87be6d4a72e970df87638fd74825e66e575e39bf
Pierre VERT <vert@obs-mip.fr>
2022-11-22 18:19:01 +0100

    prepare option page ACF

---
+++ inc/sedoo-wppl-robotstxt-acf-fields.php
@@ -0,0 +1,70 @@
+<?php
+if( function_exists('acf_add_local_field_group') ):
+
+    acf_add_local_field_group(array(
+        'key' => 'group_637cbad90befb',
+        'title' => 'Robots.txt lines',
+        'fields' => array(
+            array(
+                'key' => 'field_637cbae440df5',
+                'label' => 'Lignes à ajouter',
+                'name' => 'robots-txt-lines',
+                'type' => 'repeater',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'collapsed' => '',
+                'min' => 0,
+                'max' => 0,
+                'layout' => 'table',
+                'button_label' => 'Ajouter une ligne',
+                'sub_fields' => array(
+                    array(
+                        'key' => 'field_637cbb0f40df6',
+                        'label' => '',
+                        'name' => 'robots-txt-line',
+                        'type' => 'text',
+                        'instructions' => 'Attention, la modification du fichier robots.txt requiert une certaine expertise... <a href="https://developers.google.com/search/docs/crawling-indexing/robots/intro?hl=fr">Lire un peu de doc avant...</a>',
+                        'required' => 0,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'default_value' => '',
+                        'placeholder' => 'Sitemap: https://www.example.com/sitemap.xml',
+                        'prepend' => '',
+                        'append' => '',
+                        'maxlength' => '',
+                    ),
+                ),
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'options_page',
+                    'operator' => '==',
+                    'value' => 'acf-options-manage-robots-txt',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+        'show_in_rest' => 0,
+    ));
+    
+    endif;		
+?>
--- sedoo-wppl-robotstxt.php
+++ sedoo-wppl-robotstxt.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Sedoo Robots.txt generator
  * Description: Ajoutez des infos au fichier robots.txt
- * Version: 0.1.0
+ * Version: 0.2.0
  * Author: Pierre Vert - SEDOO OMP
  * Text Domain:  sedoo-wppl-robotstxt 
  * Domain Path:  /languages
@@ -10,10 +10,13 @@
 
  // Edit robots.txt for individual sites in a WordPress Multisite installation
 
+include 'inc/sedoo-wppl-robotstxt-acf-fields.php';
+include 'inc/sedoo-wppl-robotstxt-options.php';
+
 function sedoo_wppl_robotstxt_custom_robots( $output, $public ) {
 	$site_id = get_current_blog_id();
 	// if ( $site_id == 1 ) {
-		$output .= "Allow: /wp-content/uploads/\n";
+		// $output .= "Allow: /wp-content/uploads/\n";
 		$output .= "\nSitemap: https://api.sedoo.fr/sedoo-resif-rest/sitemap/v1_0/network_sitemap.xml";
 	// }
     //    elseif ( $site_id == 2 ) { 
